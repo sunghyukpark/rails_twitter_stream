06d1ba0ee9ab60d601c5eca4037a99783216ef90
sunghyukpark <sunghyukpark0408@gmail.com>
2016-02-06 18:06:52 -0600

    add tweets controller, views

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,14 @@
+class TweetsController < ApplicationController
+  # display all tweets with given word
+  def index
+  end
+
+  # create instances of tweets while streaming
+  def create
+  end
+
+  # destroy instances of tweets
+  def destroy
+  end
+
+end
